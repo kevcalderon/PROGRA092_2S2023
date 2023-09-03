cfb9b1ac235d165ee7d1d2cf546617f7143c4f03
Kevin Calderón <josue_29c@hotmail.com>
2023-09-02 23:44:34 -0600

    [add] ejemplo ventanas y conexion bd

---
+++ claseExtra/Login.vb
@@ -0,0 +1,25 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Login
+    Dim connection As MySqlConnection
+    Dim comandos As MySqlCommand
+
+    Private Sub buttonIrLogin_Click(sender As Object, e As EventArgs) Handles buttonIrLogin.Click
+        Me.Hide()
+        Registro.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        connection = New MySqlConnection
+        connection.ConnectionString = "server=localhost;userId=root;password='';database=progra092"
+        Try
+            connection.Open()
+            MsgBox("Conexion correctamente a BD!!!! :)")
+            connection.Close()
+        Catch ex As Exception
+            MsgBox("Error al conectarme, no funciono :C")
+            connection.Close()
+        End Try
+
+    End Sub
+End Class
---
+++ claseExtra/Registro.vb
@@ -0,0 +1,6 @@
+﻿Public Class Registro
+    Private Sub buttonBackRegistro_Click(sender As Object, e As EventArgs) Handles buttonBackRegistro.Click
+        Me.Hide()
+        Login.Show()
+    End Sub
+End Class
