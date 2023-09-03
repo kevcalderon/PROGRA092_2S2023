b0269d1ac0cd1937f213a421ba59b98eff40478c
Kevin Calderón <josue_29c@hotmail.com>
2023-09-02 23:43:41 -0600

    [ADD] clase 4 while loop

---
+++ clase4/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cicloButton = New System.Windows.Forms.Button()
+        Me.listBoxCiclos = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'cicloButton
+        '
+        Me.cicloButton.Location = New System.Drawing.Point(226, 100)
+        Me.cicloButton.Name = "cicloButton"
+        Me.cicloButton.Size = New System.Drawing.Size(175, 65)
+        Me.cicloButton.TabIndex = 0
+        Me.cicloButton.Text = "Hacer ciclo"
+        Me.cicloButton.UseVisualStyleBackColor = True
+        '
+        'listBoxCiclos
+        '
+        Me.listBoxCiclos.FormattingEnabled = True
+        Me.listBoxCiclos.ItemHeight = 20
+        Me.listBoxCiclos.Location = New System.Drawing.Point(55, 227)
+        Me.listBoxCiclos.Name = "listBoxCiclos"
+        Me.listBoxCiclos.Size = New System.Drawing.Size(711, 164)
+        Me.listBoxCiclos.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.listBoxCiclos)
+        Me.Controls.Add(Me.cicloButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cicloButton As Button
+    Friend WithEvents listBoxCiclos As ListBox
+End Class
---
+++ clase4/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub cicloButton_Click(sender As Object, e As EventArgs) Handles cicloButton.Click
+
+        'Ejercicio FOR
+        Dim i As Integer = 0
+        Dim totalRepeticion As Integer = 10
+
+        'incremental
+        'For i = 1 To totalRepeticion
+        '    listBoxCiclos.Items.Add($"Esta linea ha sido repetida {i} veces")
+        'Next
+
+        'decrement
+        'For x As Integer = 25 To 0 Step -5
+        '    listBoxCiclos.Items.Add($"Esta linea ha sido repetida {x} veces")
+        'Next
+
+
+        Dim suma As Integer = 0
+        Dim contador As Integer = 0
+        Do While suma < 100
+            ' 1era. suma = 0 + 20 = 20
+            ' 2da. suma = 20 + 20 = 40
+            ' 3ra. suma = 40 + 20 = 60
+            ' 4ta. suma = 60 + 20 = 80
+            ' 5ta. suma = 80 + 20 = 100
+            suma = suma + 20
+            '1era. contador = 0 + 1 = 1
+            '2da. contador = 1 +1 = 2
+            '3era. contador = 2+1=3
+            '4ta. contador = 3+1=4
+            '5ta. contador = 4 +1 = 5
+            contador = contador + 1
+        Loop
+        listBoxCiclos.Items.Add($"Este numero se ha sumado {contador} veces en este ciclo do while para llegar a 100.")
+
+    End Sub
+End Class
